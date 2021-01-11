eb762feb5c746a4e29e5105a975bcb07ce279d45
ivan-lytvynets <ivlitvinets@gmail.com>
2021-01-11 10:40:13 +0200

    edited Injector

--- src/main/java/core/basesyntax/lib/Injector.java
+++ src/main/java/core/basesyntax/lib/Injector.java
@@ -15,22 +15,22 @@ public class Injector {
             IllegalAccessException, InvocationTargetException, InstantiationException {
         Constructor constructor = clazz.getDeclaredConstructor();
         Object instance = constructor.newInstance();
-
-        Class<Dao> annotationDao = Dao.class;
-
         Field[] declaredFields = clazz.getDeclaredFields();
         for (Field field : declaredFields) {
-            if (field.getAnnotation(Inject.class) != null) {
+            if (field.isAnnotationPresent(Inject.class)) {
                 field.setAccessible(true);
-                if (BetDaoImpl.class.isAnnotationPresent(annotationDao)
-                        && UserDaoImpl.class.isAnnotationPresent(annotationDao)) {
-                    if (field.getType() == BetDao.class) {
-                        field.set(instance, Factory.getBetDao());
-                    } else if (field.getType() == UserDao.class) {
-                        field.set(instance, Factory.getUserDao());
-                    }
-                } else {
-                    throw new AnnotationException("Annotation doesn't exist");
+                Object objectDao = new Object();
+                if (field.getType() == BetDao.class) {
+                    objectDao = Factory.getBetDao();
+                } else if (field.getType() == UserDao.class) {
+                    objectDao = Factory.getUserDao();
+                }
+                if (objectDao.getClass().isAnnotationPresent(Dao.class)) {
+                    field.set(instance, objectDao);
+                }
+                else {
+                    throw new AnnotationException("Annotation @Dao doesn't exist in class "
+                            + objectDao.getClass());
                 }
             }
         }
